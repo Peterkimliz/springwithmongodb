18f514e747232026b14ca3cfa695e0c8e16e40f0
PeterKimLiz <pkironji@yahoo.com>
2022-09-23 13:53:09 +0300

    controller advice

--- src/main/java/com/example/StroreApp/Advice/Advice.java
+++ src/main/java/com/example/StroreApp/Advice/Advice.java
@@ -1,9 +1,25 @@
 package com.example.StroreApp.Advice;
 
+import com.example.StroreApp.Exceptions.ExceptionObject;
+import com.example.StroreApp.Exceptions.NotFoundResource;
+import com.example.StroreApp.Exceptions.ResourceExistException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class Advice {
+ @ExceptionHandler( ResourceExistException.class)
+    public ExceptionObject resourceFound(ResourceExistException e){
+     ExceptionObject exceptionObject=new ExceptionObject();
+     exceptionObject.setMessage(e.getMessage());
+     return exceptionObject;
+ }
+ @ExceptionHandler( NotFoundResource.class)
+    public ExceptionObject resourceNotFound(NotFoundResource e){
+     ExceptionObject exceptionObject=new ExceptionObject();
+     exceptionObject.setMessage(e.getMessage());
+     return exceptionObject;
+ }
 
 
 }
